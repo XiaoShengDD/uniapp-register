366539f521e1c8f11c2009d5bd14319b499f6fc8
wcs <wcs419@126.com>
2021-10-31 23:52:13 +0800

    wcs 优化

--- config/index.js
+++ config/index.js
@@ -1,5 +1,8 @@
 let baseUrl = ''
-baseUrl = 'http://app.leshichina.net/api/oxfam/v1.0'
+// baseUrl = 'http://app.leshichina.net/api/oxfam/v1.0' // 正式环境
+baseUrl = 'http://18.166.14.56:60000/api/oxfam/v1.0' // 测试环境
+const up_img = baseUrl+'/image/upload'
 module.exports = {
 	baseUrl,
+	up_img
 }
